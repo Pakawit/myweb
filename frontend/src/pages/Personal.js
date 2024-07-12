ce012fc537dfd64b359ab1835bd8512a4b38dd98
Pakawit <s6304062616234@email.kmutnb.ac.th>
2024-07-12 20:56:25 +0700

    12/7/2024
    12/7/2024 เพื่อตรวจสอบข้อและป๊อปอัพแสดงเสร็จสิ้นหน้าข้อมูลส่วนบุคคล

--- frontend/src/pages/Personal.js
+++ frontend/src/pages/Personal.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Container, Button, Form, Row, Col } from "react-bootstrap";
+import { Container, Button, Form, Row, Col, Modal, Alert } from "react-bootstrap";
 import Navigation from "../components/Navigation";
 import { AppContext } from "../context/appContext";
 import { useDispatch, useSelector } from "react-redux";
@@ -12,8 +12,10 @@ function Personal() {
   const medication = useSelector((state) => state.medication) || [];
   const { API_BASE_URL } = useContext(AppContext);
   const [member, setMember] = useState(selectuser);
+  const [errors, setErrors] = useState({});
   const dispatch = useDispatch();
   const [editMode, setEditMode] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     dispatch(fetchMedicationsThunk());
@@ -27,144 +29,94 @@ function Personal() {
     }));
   }, [medication, selectuser._id]);
 
-  const handleChange = (name, value) => {
-    setMember((prevMember) => ({
-      ...prevMember,
-      [name]: value,
-    }));
+  const validate = (name, value) => {
+    let error = "";
+    switch (name) {
+      case "name":
+        if (!/^[a-zA-Zก-๙\s]*$/.test(value) || value.length > 30) {
+          error = "ต้องเป็นตัวอักษรเท่านั้น ไม่เกิน 30 ตัว";
+        }
+        break;
+      case "phone":
+      case "other_numbers":
+        if (!/^\d*$/.test(value) || value.length > 10) {
+          error = "ต้องเป็นตัวเลขเท่านั้น ไม่เกิน 10 ตัว";
+        }
+        break;
+      case "age":
+        if (!/^\d*$/.test(value) || value.length > 3) {
+          error = "ต้องเป็นตัวเลขเท่านั้น ไม่เกิน 3 ตัว";
+        }
+        break;
+      case "diagnosis":
+      case "taking_capecitabine":
+      case "other_medicine":
+        if (value.length > 300) {
+          error = "ต้องไม่เกิน 300 ตัว";
+        }
+        break;
+      case "hospital_number":
+        if (value.length > 50) {
+          error = "ต้องไม่เกิน 50 ตัว";
+        }
+        break;
+      default:
+        break;
+    }
+    setErrors((prevErrors) => ({ ...prevErrors, [name]: error }));
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    validate(name, value);
+    setMember((prevMember) => ({ ...prevMember, [name]: value }));
   };
 
   const handleSubmit = async () => {
-    if (editMode) {
+    if (Object.values(errors).every((err) => err === "")) {
       axios
         .put(`${API_BASE_URL}/update`, member)
         .then((res) => {
           dispatch(setselectuser(res.data));
           setEditMode(false);
+          setShowModal(true);
         })
         .catch((err) => console.log(err));
     }
-  };
-
-  const handleEditClick = () => {
-    setEditMode(true);
   };
 
   return (
     <Container>
       <Navigation />
       <h1>ข้อมูลส่วนบุคคล</h1>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group as={Row} className="mb-3">
-          <Form.Label column sm="6" style={{ textAlign: "center" }}>
-            ชื่อ-สกุล
-          </Form.Label>
-          <Col sm="6">
-            <Form.Control
-              type="text"
-              name="name"
-              value={member.name}
-              onChange={(e) => handleChange("name", e.target.value)}
-              disabled={!editMode}
-            />
-          </Col>
-        </Form.Group>
-
-        <Form.Group as={Row} className="mb-3">
-          <Form.Label column sm="6" style={{ textAlign: "center" }}>
-            เบอร์โทรศัพท์
-          </Form.Label>
-          <Col sm="6">
-            <Form.Control
-              type="text"
-              name="phone"
-              value={member.phone}
-              onChange={(e) => handleChange("phone", e.target.value)}
-              disabled={!editMode}
-            />
-          </Col>
-        </Form.Group>
-
-        <Form.Group as={Row} className="mb-3">
-          <Form.Label column sm="6" style={{ textAlign: "center" }}>
-            เบอร์โทรศัพท์ผู้ติดต่อ
-          </Form.Label>
-          <Col sm="6">
-            <Form.Control
-              type="text"
-              name="other_numbers"
-              value={member.other_numbers}
-              onChange={(e) => handleChange("other_numbers", e.target.value)}
-              disabled={!editMode}
-            />
-          </Col>
-        </Form.Group>
-
-        <Form.Group as={Row} className="mb-3">
-          <Form.Label column sm="6" style={{ textAlign: "center" }}>
-            อายุ
-          </Form.Label>
-          <Col sm="6">
-            <Form.Control
-              type="number"
-              name="age"
-              value={member.age}
-              onChange={(e) => handleChange("age", e.target.value)}
-              disabled={!editMode}
-            />
-          </Col>
-        </Form.Group>
-
-        <Form.Group as={Row} className="mb-3">
-          <Form.Label column sm="6" style={{ textAlign: "center" }}>
-            การวินิจฉัยโรคหลัก
-          </Form.Label>
-          <Col sm="6">
-            <Form.Control
-              type="text"
-              as="textarea"
-              name="diagnosis"
-              value={member.diagnosis}
-              onChange={(e) => handleChange("diagnosis", e.target.value)}
-              disabled={!editMode}
-            />
-          </Col>
-        </Form.Group>
-
-        <Form.Group as={Row} className="mb-3">
-          <Form.Label column sm="6" style={{ textAlign: "center" }}>
-            การรับประทานยา Capecitabine
-          </Form.Label>
-          <Col sm="6">
-            <Form.Control
-              type="text"
-              as="textarea"
-              name="taking_capecitabine"
-              value={member.taking_capecitabine}
-              onChange={(e) =>
-                handleChange("taking_capecitabine", e.target.value)
-              }
-              disabled={!editMode}
-            />
-          </Col>
-        </Form.Group>
-
-        <Form.Group as={Row} className="mb-3">
-          <Form.Label column sm="6" style={{ textAlign: "center" }}>
-            ยาที่ใช้ร่วม
-          </Form.Label>
-          <Col sm="6">
-            <Form.Control
-              type="text"
-              as="textarea"
-              name="other_medicine"
-              value={member.other_medicine}
-              onChange={(e) => handleChange("other_medicine", e.target.value)}
-              disabled={!editMode}
-            />
-          </Col>
-        </Form.Group>
-
+      <Form onSubmit={(e) => e.preventDefault()}>
+        {[
+          { label: "ชื่อ-สกุล", name: "name" },
+          { label: "เบอร์โทรศัพท์", name: "phone" },
+          { label: "เบอร์โทรศัพท์ผู้ติดต่อ", name: "other_numbers" },
+          { label: "อายุ", name: "age", type: "number" },
+          { label: "การวินิจฉัยโรคหลัก", name: "diagnosis", as: "textarea" },
+          { label: "การรับประทานยา Capecitabine", name: "taking_capecitabine", as: "textarea" },
+          { label: "ยาที่ใช้ร่วม", name: "other_medicine", as: "textarea" },
+          { label: "เลขโรงพยาบาล", name: "hospital_number" }
+        ].map((field, index) => (
+          <Form.Group as={Row} className="mb-3" key={index}>
+            <Form.Label column sm="6" style={{ textAlign: "center" }}>
+              {field.label}
+            </Form.Label>
+            <Col sm="6">
+              <Form.Control
+                type={field.type || "text"}
+                as={field.as}
+                name={field.name}
+                value={member[field.name] || ""}
+                onChange={handleChange}
+                disabled={!editMode}
+              />
+              {errors[field.name] && <Alert variant="danger">{errors[field.name]}</Alert>}
+            </Col>
+          </Form.Group>
+        ))}
         <Form.Group as={Row} className="mb-3">
           <Form.Label column sm="6" style={{ textAlign: "center" }}>
             ขาดยา
@@ -173,45 +125,39 @@ function Personal() {
             <Form.Control
               type="text"
               name="ms_medicine"
-              value={member.ms_medicine}
+              value={member.ms_medicine || ""}
               disabled
             />
           </Col>
         </Form.Group>
-
-        <Form.Group as={Row} className="mb-3">
-          <Form.Label column sm="6" style={{ textAlign: "center" }}>
-            เลขโรงพยาบาล
-          </Form.Label>
-          <Col sm="6">
-            <Form.Control
-              type="text"
-              name="hospital_number"
-              value={member.hospital_number}
-              onChange={(e) => handleChange("hospital_number", e.target.value)}
-              disabled={!editMode}
-            />
-          </Col>
-        </Form.Group>
-
         <Form.Group as={Row} className="mb-3">
           <Col
             sm={{ span: 6, offset: 6 }}
             className="d-flex justify-content-end"
           >
             {editMode ? (
-              <Button
-                variant="outline-success"
-                type="button"
-                onClick={handleSubmit}
-              >
-                บันทึก
-              </Button>
+              <>
+                <Button
+                  variant="outline-success"
+                  type="button"
+                  onClick={handleSubmit}
+                >
+                  บันทึก
+                </Button>
+                <Button
+                  variant="outline-danger"
+                  type="button"
+                  onClick={() => setEditMode(false)}
+                  style={{ marginLeft: "10px" }}
+                >
+                  ยกเลิก
+                </Button>
+              </>
             ) : (
               <Button
                 variant="outline-dark"
                 type="button"
-                onClick={handleEditClick}
+                onClick={() => setEditMode(true)}
               >
                 แก้ไข
               </Button>
@@ -219,6 +165,17 @@ function Personal() {
           </Col>
         </Form.Group>
       </Form>
+      <Modal show={showModal} onHide={() => setShowModal(false)} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>การแก้ไขเสร็จสิ้น</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>แก้ไขข้อมูลเรียบร้อยแล้ว</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowModal(false)}>
+            ปิด
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Container>
   );
 }
