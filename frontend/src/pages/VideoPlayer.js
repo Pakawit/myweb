05af379b1405fac931c1f920b5846c0f85e3c37f
Pakawit <s6304062616234@email.kmutnb.ac.th>
2024-11-11 15:53:49 +0700

    11/11/2024 แก้ url หน้า video

--- frontend/src/pages/VideoPlayer.js
+++ frontend/src/pages/VideoPlayer.js
@@ -5,7 +5,7 @@ function VideoPlayer() {
     <div className="container-fluid d-flex justify-content-center align-items-center vh-100">
       <div className="ratio ratio-16x9" style={{ width: "100%" }}>
         <video controls style={{ width: "100%", height: "100%" }}>
-          <source src="http://chaibwoot.no-ip.org:4452/video" type="video/mp4" />
+          <source src="http://localhost:4452/video" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
